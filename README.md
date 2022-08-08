253f474cbe4ecdfa82fd3ddf929a1db05606d23f
Anna Miklukha <miklukha.anna@ukr.net>
2022-08-08 23:36:26 +0300

    add homework

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # goit-node-hw-01
+
+![list](./img/list.png) ![add contact](./img/add-contact.png)
+![get contact](./img/get-contact.png)
+![remove contact](./img/remove-contact.png)
---
+++ contacts.js
@@ -0,0 +1,55 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+
+  if (!~index) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  console.log(result);
+  await updateContacts(contacts);
+  return result;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  updateContacts(contacts);
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
